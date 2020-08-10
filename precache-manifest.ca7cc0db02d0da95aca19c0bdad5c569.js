bf02a232b47515010ea0017dfb0f2fd49ef96329
Anton Shafner <ashafner@tsconsulting.com>
2020-08-10 17:41:46 +0300

    Updates

---
+++ precache-manifest.ca7cc0db02d0da95aca19c0bdad5c569.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fa76eb689ee531ca27c2f0b9791ae0",
+    "url": "/table-editor/index.html"
+  },
+  {
+    "revision": "279203aed73155f14b29",
+    "url": "/table-editor/static/css/2.8ff17cee.chunk.css"
+  },
+  {
+    "revision": "2ffff4832c9ffd88945b",
+    "url": "/table-editor/static/css/main.7062aef7.chunk.css"
+  },
+  {
+    "revision": "279203aed73155f14b29",
+    "url": "/table-editor/static/js/2.686fc1d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/table-editor/static/js/2.686fc1d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ffff4832c9ffd88945b",
+    "url": "/table-editor/static/js/main.87401e29.chunk.js"
+  },
+  {
+    "revision": "924731641ea28ec6346f",
+    "url": "/table-editor/static/js/runtime-main.9eb10d51.js"
+  },
+  {
+    "revision": "627b2ee21cd6432724468a367c922bfb",
+    "url": "/table-editor/static/media/delete.627b2ee2.svg"
+  },
+  {
+    "revision": "d37d94008347a67f103811601107af7f",
+    "url": "/table-editor/static/media/pencil-edit.d37d9400.svg"
+  },
+  {
+    "revision": "52d7af7770a321a065287c88ed5f2830",
+    "url": "/table-editor/static/media/up-arrow.52d7af77.svg"
+  }
+]);
